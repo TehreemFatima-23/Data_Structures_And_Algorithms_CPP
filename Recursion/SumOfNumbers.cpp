25dcde59a4ce6a9fcbe3e2ec77b34bb697f4ddf2
TehreemFatima-23 <tehreem.twice2005@gmail.com>
2026-07-01 18:30:24 +0500

    Program to sum the numbers upto n

---
+++ Recursion/SumOfNumbers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int sum(int n)
+{
+    if (n == 0)
+        return 0;
+
+    // Recursive case
+    return n + sum(n - 1);
+}
+
+int main()
+{
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    cout << "Sum = " << sum(n);
+    return 0;
+}
